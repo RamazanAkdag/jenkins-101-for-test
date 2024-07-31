e0f5d5f46b64cf43aaac47ae2f6af0579185fe82
Ramazan Fırat Akdağ <97161858+RamazanAkdag@users.noreply.github.com>
2024-07-31 13:27:14 +0300

    Update hello.py

--- myapp/hello.py
+++ myapp/hello.py
@@ -1,7 +1,11 @@
 import fire
+import time
 
 def hello(name="World"):
   return "Hello %s!" % name
 
 if __name__ == '__main__':
   fire.Fire(hello)
+  for i in range(5):
+    print(i)
+    time.sleep(2)
